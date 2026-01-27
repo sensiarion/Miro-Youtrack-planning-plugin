447c81aaa745c8cb8f4497282d4c9a68de56ff9d
sensiarion <izertmi@gmail.com>
2026-01-27 07:47:25 +0500

    fix ci

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,12 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        app: './app.html',
+      },
+    },
+  },
 });
